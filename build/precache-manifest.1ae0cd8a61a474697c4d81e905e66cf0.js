0bea0357d68770aece751890b826cfe46f8f1302
Henri jeremie <jeremie.henri@etu.univ-amu.fr>
2020-04-13 11:29:41 +0200

    Add files via upload

---
+++ build/precache-manifest.1ae0cd8a61a474697c4d81e905e66cf0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "720c89befb0c4e5e5e7b8213d1d2c557",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7ccc8983675ae59eb88f",
+    "url": "./static/css/main.ec86ce50.chunk.css"
+  },
+  {
+    "revision": "e2b53d97413e83a3a242",
+    "url": "./static/js/2.8d5b99a7.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "./static/js/2.8d5b99a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ccc8983675ae59eb88f",
+    "url": "./static/js/main.4ef5360c.chunk.js"
+  },
+  {
+    "revision": "2533407d2dd3faaddc91",
+    "url": "./static/js/runtime-main.103ef872.js"
+  }
+]);
