59f84ff4c604ccef3ae0a0c6c0c90241e3bb6829
Henri jeremie <jeremie.henri@etu.univ-amu.fr>
2020-04-13 13:15:43 +0200

    Add files via upload

--- src/Chat.js
+++ src/Chat.js
@@ -1,32 +1,32 @@
-import React, { Component } from 'react'
-import ChatInput from './ChatInput'
-import ChatMessage from './ChatMessage'
+import React, { Component } from 'react';
+import ChatInput from './ChatInput';
+import ChatMessage from './ChatMessage';
 import UserStore from "./stores/UserStore";
 
-const URL = 'ws://localhost:3030'
+const URL = "ws://www.jeremie-henri-test.glitch.me:8080";
 
 class Chat extends Component {
     state = {
         name: UserStore.username,
         messages: [],
-    }
+    };
 
-    ws = new WebSocket(URL)
+    ws = new WebSocket(URL);
 
     componentDidMount() {
         this.ws.onopen = () => {
             // on connecting, do nothing but log it to the console
             console.log('connected')
-        }
+        };
 
         this.ws.onmessage = evt => {
             // on receiving a message, add it to the list of messages
-            const message = JSON.parse(evt.data)
+            const message = JSON.parse(evt.data);
             this.addMessage(message)
-        }
+        };
 
         this.ws.onclose = () => {
-            console.log('disconnected')
+            console.log('disconnected');
             // automatically try to reconnect on connection loss
             this.setState({
                 ws: new WebSocket(URL),
@@ -35,14 +35,14 @@ class Chat extends Component {
     }
 
     addMessage = message =>
-        this.setState(state => ({ messages: [message, ...state.messages] }))
+        this.setState(state => ({ messages: [message, ...state.messages] }));
 
     submitMessage = messageString => {
         // on submitting the ChatInput form, send the message, add it to the list and reset the input
-        const message = { name: UserStore.username, message: messageString }
-        this.ws.send(JSON.stringify(message))
+        const message = { name: UserStore.username, message: messageString };
+        this.ws.send(JSON.stringify(message));
         this.addMessage(message)
-    }
+    };
 
     render() {
         return (
--- src/ChatInput.js
+++ src/ChatInput.js
@@ -1,21 +1,21 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class ChatInput extends Component {
     static propTypes = {
         onSubmitMessage: PropTypes.func.isRequired,
-    }
+    };
     state = {
         message: '',
-    }
+    };
 
     render() {
         return (
             <form
                 action="."
                 onSubmit={e => {
-                    e.preventDefault()
-                    this.props.onSubmitMessage(this.state.message)
+                    e.preventDefault();
+                    this.props.onSubmitMessage(this.state.message);
                     this.setState({ message: '' })
                 }}
             >
