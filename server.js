605c67d4c438fac03a84aa9b56bb7df73bd09d91
Henri jeremie <jeremie.henri@etu.univ-amu.fr>
2020-04-13 13:15:03 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -6,8 +6,9 @@ const session = require('express-session');
 const MySQLStore = require('express-mysql-session')(session);
 const Router = require('./Router');
 const WebSocket = require('ws');
+const WebSocketServer = require('ws').Server;
 
-const wss = new WebSocket.Server({ port: 3030 });
+const wss = new WebSocketServer({port:8080});
 
 wss.on('connection', function connection(ws) {
     ws.on('message', function incoming(data) {
